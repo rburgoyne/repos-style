7029d40499371d4a645e9c334f91584fbe591994
solsson <solsson@5a4de678-0f31-0410-84e4-bbaf904ce06c>
2008-03-29 18:18:48 +0000

    First attempt at a multi-repo solution that does not require changes to repos.xsl. Improved the documentarion of config options.
    git-svn-id: https://labs.repos.se/data/style/trunk@400 5a4de678-0f31-0410-84e4-bbaf904ce06c

--- repos-web/open/log/index.php
+++ repos-web/open/log/index.php
@@ -1,30 +1,73 @@
 <?php
 /**
- * Repos Style log reader (c) 2007 Staffan Olsson www.reposstyle.com
- * 
+ * Repos Style log reader (c) 2007-2008 Staffan Olsson reposstyle.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 // === Print svn log --xml to response ===
-$repo = '@@Repository@@'; // repository root, no trailing slash
-$limit = 20; // limit log length for performance reasons, advice users to run svn client for more entries 
 
-// === configuration done ===
+// Set the URL to the stylesheet, must be same host or absolute path from root
+$xslt = '/repos-web/view/log.xsl';
+
+// Set either repository root url OR the url to SVNParentPath for multi-repo
+// accepts any subversion url that does not require authentication, including file:///
+// no trailing slash
+$repo = '@@Repository@@';
+// OR set a parent url instead, corresponding to SVNParentPath, no trailing slash
+//$repoparent = '@@RepoParent@@';
+$repoparent = 'http://localhost/data';
+
+// limit log length for performance reasons (users should run svn client for more entries)
+$limit = 20;
+
+// === configuration done, get parameters ===
+
+isset($_REQUEST['target']) or die("Parameter 'target' is required");
+$target = $_REQUEST['target'];
+
+// Repo may be set as server environment variable
+// This allows configuration without editing this file,
+// as well as very flexible url resolution from mod_rewrite
+if (strstr($repo,'@@') && isset($_SERVER['ReposRepo'])) $repo = $_SERVER['ReposRepo'];
+
+// For multi-repo, try to resolve url to be used in svn command (it is not known by the xslt)
+if (!strstr($repoparent,'@@') && strstr($repo,'@@')) {
+	if (isset($_REQUEST['reponame'])) {
+		$repo = $repoparent.'/'.$_REQUEST['reponame'];
+	} else if (isset($_SERVER['HTTP_REFERER'])) {
+		$referer = $_SERVER['HTTP_REFERER'];
+		preg_match('/\/([^\/]+)'.preg_quote($target,'/').'\/?$/',$referer,$matches);
+		print_r($referer);
+		if (count($matches)>1) {
+			$reponame = $matches[1];
+			// make bookmarkable if possible
+			if (isset($_SERVER['SCRIPT_URI'])) {
+				header('Location: '.$_SERVER['SCRIPT_URI'].'?target='.rawurlencode($target).'&reponame='.$reponame);
+				exit;
+			}
+			// not bookmarkable, continue with referer
+			$repo = $repoparent.'/'.$reponame;
+		}
+	}
+	if (strstr($repo,'@@')) die('Unable to resolve repository name for this request, reponame must be set');
+}
+
+// === validate and run svn ===
 if (strstr($repo,'@@')) die('The log script must be configured with a root URL');
 is_numeric($limit) or die('The log script must be configured with a numeric limit');
 
-isset($_REQUEST['target']) or die("Parameter 'target' is required");
-$url = $repo . $_REQUEST['target'];
+$url = $repo . $target;
 
 // command line, injection safe
 $cmd = 'svn log --xml --verbose --incremental --non-interactive';
@@ -34,7 +77,7 @@ $cmd .= ' 2>&1';
 
 header('Content-Type: text/xml');
 echo('<?xml version="1.0"?>
-<?xml-stylesheet type="text/xsl" href="/repos-web/view/log.xsl"?>
+<?xml-stylesheet type="text/xsl" href="'.$xslt.'"?>
 <log limit="'.$limit.'">
 ');
 passthru($cmd);
